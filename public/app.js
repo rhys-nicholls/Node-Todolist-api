96792bf0799fe9dc06a71d2ccd763e49f44e5e82
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-02-21 19:30:47 +0000

    Add functionality to add new todo item
    - Checks for enter key press
    - Send post request to /api/todos taking value from form
    - Add todo to li (adding todo to li now in separate function, addto)

--- public/app.js
+++ public/app.js
@@ -1,20 +1,44 @@
 $(document).ready(function () {
     $.getJSON("/api/todos")
         .then(addTodos);
+
+    $('#todoInput').keypress(function (event) {
+        if(event.which === 13){
+            createTodo();
+        }
+    })
 });
 
 function addTodos(todos) {
 
     //add todos to the page
     todos.forEach(function (todo) {
-
-        //Create li with class task
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
-        if(todo.complete){
-           newTodo.addClass('done');
-        }
-
-        //append to li with class list
-        $('.list').append(newTodo);
+        addTodo(todo);
     });
+}
+
+function addTodo(todo) {
+
+    //Create li with class task
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    if(todo.complete){
+        newTodo.addClass('done');
+    }
+
+    //append to li with class list
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+
+    //send request to create new todo
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput} )
+        .then(function (newTodo) {
+            $('#todoInput').val('');
+            addTodo(newTodo);
+        })
+        .catch(function (err) { 
+            console.log(err);
+        })
 }
